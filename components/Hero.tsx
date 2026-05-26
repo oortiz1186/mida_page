bf0872ca988251faae8c0e7adcdf4196a0c1e2b2
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-05-26 10:09:08 -0600

    feat: implementacion global de whatsapp en el layout principal

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       {/* IMAGEN DE FONDO ABSOLUTA */}
       <div className="absolute inset-0 z-0">
         <Image 
-          src="/images/OFICINA.PNG" 
+          src="/images/OFICINA.png" 
           alt="Oficina MIDA"
           fill
           priority
