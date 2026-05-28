aed60ff9e1287e5e3d1e76c7b923a4729eaf63a4
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-05-27 19:19:37 -0600

    se arreglos carcha

--- components/config/empresa.ts
+++ components/config/empresa.ts
@@ -3,7 +3,7 @@ export const infoEmpresa = {
   legal: "MIDA",
   dominio: "https://mida-page-qvwj.vercel.app",
   formspreeId: "xnjrojny",
-  recaptchaSiteKey: "6LdSBQAtAAAAANsB36o0domKFmApPn2Q-MYpk-PC",
+  recaptchaSiteKey: "6LcnGwAtAAAAABFZ6OsWkCOoOWPH4kDD3rFbdw-R",
 
   // Teléfonos y WhatsApp (Modifica aquí tus números reales)
   telefonoTexto: "477 329 5727",         // Cómo se ve en la pantalla
