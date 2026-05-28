32d986b260044693440f150e4bca407ab534e66b
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-05-28 17:00:28 -0600

    implementacin de whatsapp corporativivo fase 1

--- app/api/webhook/whatsapp/route.ts
+++ app/api/webhook/whatsapp/route.ts
@@ -1,21 +1,43 @@
 import { NextResponse } from "next/server";
 
 async function enviarMensajeWhatsApp(numero: string, texto: string) {
-  // tu función aquí
+
+  const url =
+    `${process.env.EVOLUTION_API_URL}/message/sendText/${process.env.EVOLUTION_INSTANCE_NAME}`;
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: process.env.EVOLUTION_API_KEY as string
+    },
+    body: JSON.stringify({
+      number: numero,
+      options: {
+        delay: 1200,
+        presence: "composing"
+      },
+      textMessage: {
+        text: texto
+      }
+    })
+  });
+
 }
 
 export async function POST(req: Request) {
+
   try {
 
     const body = await req.json();
 
     console.log(JSON.stringify(body, null, 2));
 
-    // EXTRAER NUMERO
+    // IMPORTANTE:
+    // NO cortar el @g.us temporalmente
     const numeroCliente =
-      body?.data?.key?.remoteJid?.split("@")[0];
+      body?.data?.key?.remoteJid;
 
-    // ENVIAR JSON AL WHATSAPP
     await enviarMensajeWhatsApp(
       numeroCliente,
       JSON.stringify(body, null, 2).slice(0, 3000)
@@ -34,4 +56,5 @@ export async function POST(req: Request) {
     });
 
   }
+
 }
