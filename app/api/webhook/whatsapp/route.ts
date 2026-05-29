6b02bad8173b4612be0e7652ffcf08a5d28f0fa7
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-05-29 13:09:29 -0600

    cambio de modelo de IA

--- app/api/webhook/whatsapp/route.ts
+++ app/api/webhook/whatsapp/route.ts
@@ -8,7 +8,7 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 async function generarRespuestaIA(mensajeUsuario: string) {
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8b:generateContent",
     {
       method: "POST",
       headers: {
