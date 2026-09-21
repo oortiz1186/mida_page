0f7ad686c5fd8fb3fb4cf1fcb642c9c45ff1bc7a
Octavio Ortiz <157636731+oortiz1186@users.noreply.github.com>
2026-09-21 12:24:57 -0600

    SEO: implement MIDA website recommendations

--- components/config/empresa.ts
+++ components/config/empresa.ts
@@ -1,29 +1,20 @@
 export const infoEmpresa = {
   nombre: "MIDA Tech Consulting",
   legal: "MIDA",
-  dominio: "https://mida-page-qvwj.vercel.app",
+  dominio: "https://mida.mx",
   formspreeId: "xnjrojny",
   recaptchaSiteKey: "6LcnGwAtAAAAABFZ6OsWkCOoOWPH4kDD3rFbdw-R",
   portalSoporteUrl: "https://soporte-mida.vercel.app/login",
-
-  // Teléfonos y WhatsApp (Modifica aquí tus números reales)
-  telefonoTexto: "477 329 5727",         // Cómo se ve en la pantalla
-  telefonoEnlace: "4773295727",          // Sin espacios para que funcione el clic de llamada
-  
-  whatsappTexto: "477 188 8836",         // Cómo se ve en el Footer
-  whatsappNumero: "5214771888836",       // Código de país + número (sin espacios ni el "+")
+  telefonoTexto: "477 329 5727",
+  telefonoEnlace: "4773295727",
+  whatsappTexto: "477 188 8836",
+  whatsappNumero: "5214771888836",
   whatsappMensajePredeterminado: "Hola MIDA, necesito asesoría o soporte técnico.",
-  whatsappNumeroBot: "5214776336652",       // Código de país + número (sin espacios ni el "+")
+  whatsappNumeroBot: "5214776336652",
   whatsappMensajePredeterminadoBot: "Hola, necesito atención de MIDA",
-
-  // Ubicación
   direccionCompleta: "Blvr. Mariano Escobedo Pte. 4546 Int 402-A, San Isidro de Jerez, Centro de Negocios Plaza K2, 37530 León de los Aldama, Gto., México.",
   direccionLinea1: "Blvr. Mariano Escobedo Pte. 4546 Int 402-A, San Isidro de Jerez, Centro de Negocios Plaza K2",
   direccionLinea2: "León, Guanajuato, México.",
-  
-  // Redes o Correos por si los necesitas en el futuro
   correoContacto: "dulce@mida.mx",
-
-  // Ajuste Analítica: Añadimos tu ID de Google Analytics 4
   googleAnalyticsId: "G-XXXXXXX",
 };
